29c593d02f912e6a6d2bb2999ccb4d0b41c9a58c
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2020-08-06 16:26:07 +0530

    Add files via upload

---
+++ rec sum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define ll long long int  
+ll sum(int n) 
+{ 
+    if (n == 1) 
+        return 1; 
+    else
+        return ((ll)pow(n, n) + sum(n - 1)); 
+} 
+int main() 
+{ 
+    int n ;
+	cin>>n; 
+    cout << sum(n); 
+  
+    return 0; 
+} 
