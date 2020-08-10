7097997f8c6514c07277a80b957e22392f6fbfdd
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2020-08-10 12:40:02 +0530

    Add files via upload

---
+++ LinearSearch/two.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int search(int arr[], int n, int x) 
+{ 
+	int i; 
+	for (i = 0; i < n; i++) 
+		if (arr[i] == x) 
+			return i; 
+	return -1; 
+} 
+
+int main(void) 
+{ 
+	int arr[100],n;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	int x = 10; 
+	int result = search(arr, n, x); 
+	(result == -1) ? printf("Element is not present in array") 
+				: printf("Element is present at index %d", 
+							result); 
+	return 0; 
+} 
+
+
