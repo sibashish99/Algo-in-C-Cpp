d0c8856e1a36e327cdb35145f1bd57572586d18f
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2020-08-10 23:57:05 +0530

    Add files via upload

---
+++ Magic sq/sqrt.cpp
@@ -0,0 +1,17 @@
+#include <math.h> 
+#include <stdio.h> 
+
+double findSQRT(double N) 
+{ 
+	return sqrt(N); 
+} 
+ 
+int main() 
+{ 
+
+	int N ;
+	scanf("%d",&N); 
+	printf("%f ", findSQRT(N)); 
+	return 0; 
+} 
+
