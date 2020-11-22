72bd8186d4c2656ca615f6bd6557d7c6c27fef6d
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2020-11-22 22:44:27 +0530

    Update swap.c

--- Swap/swap.c
+++ Swap/swap.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-
-int swap(int *a,int *b)
+void swap(int *a,int *b)
 {
 	int temp=*a;
 	*a=*b;
 	*b=temp;
+}
+//alternate
+void swap(int m,int n){
+   m=m+n;  //m=10 n=20 => m=30
+   n=m-n;   //n=30-20=10
+   m=m-n;    //m=30-10=20
 }
 
 int main()
@@ -19,3 +24,5 @@ int main()
 
 
 }
+
+
