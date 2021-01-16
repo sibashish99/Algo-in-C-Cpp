0dae54ee99b9212e8e9c6cc0a795de0538a99718
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-01-16 22:23:20 +0530

    Update max and smax num in an array.cpp

--- interview prep/max and smax num in an array.cpp
+++ interview prep/max and smax num in an array.cpp
@@ -1,19 +1,33 @@
 #include<stdio.h>
+int maxF(int a[i],int n){
+   int max=0;
+	for(int i=0;i<n;i++){
+		if(a[i]>max){
+			 max=a[i];
+		}
+		
+	}
+    return max;
+}
+int secMax(int a[i],int n,int m){
+    int smax=0,;
+	for(int i=0;i<n;i++){
+		if(a[i]<m && a[i]>smax){
+                    smax=a[i];
+		}
+		
+	}
+    return max;
+}
 int main(){
 	int n,a[1000];
 	scanf("%d",&n);
 	for(int i=0;i<n;i++){
 		scanf("%d",&a[i]);
 	}
-	
+	int m=maxF(a,n)
+	printf("max is :%d",m);
+	printf("Second max is :%d",secMax(a,n,m));
 	int max=0,smax=0;
-	for(int i=0;i<n;i++){
-		if(a[i]>max){
-			 max=a[i];
-		}
-		if(a[i]>smax && a[i]<max){
-			smax=a[i];
-		}
-	}
-	printf("Max is :%d\n Smax is %d",max,smax);
+	
 }
