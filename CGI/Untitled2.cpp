da63d511eafe356c41bd840b1b7e1591f3d694c0
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-01-17 21:07:03 +0530

    Update Untitled2.cpp

--- CGI/Untitled2.cpp
+++ CGI/Untitled2.cpp
@@ -1,7 +1,18 @@
+// Input
+// The input consists of two space separated integers- data and digit, representing the data to be encoded and the digit to be counted in the data
+
+// Output
+// Print an integer representing the count of the specific digit.
+
+// Example Input:
+// 572378233 3
+// Output:
+// 3
+	
 #include<iostream>
 #include<math.h>
 using namespace std;
-int main(){   //occurance of a number in a long no
+int main(){   
 	int k;
 	long long int n;
 	cin>>n>>k;
@@ -9,6 +20,7 @@ int main(){   //occurance of a number in a long no
 	
 	while(n>0){
 		count = (n%10==k)? count+1:count; 
+		n=n/10;
 	}
         cout<<count<<endl;
 }
