712eb7b558c3b1fe005eaec5e399d680bb63df35
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-01-17 21:05:23 +0530

    Update Untitled1.cpp

--- CGI/Untitled1.cpp
+++ CGI/Untitled1.cpp
@@ -1,3 +1,22 @@
+// Input
+// The first line of the input consists of an Integer numOfCust, representing the number of customers whose bills are selected for the promotional discount (N).
+// The second line consists of N space-separated integers - bill, bill . , bill representing the bill amounts of the N customers selected for the promotional discount,
+
+// Output
+// Print an integer representing the number of customers that will be given discounts.
+
+// Constraints
+// O<= numofCusts <=105
+// 0<=bill <= 10
+// O<=numolCust
+
+// Example Input:
+// 6
+// 25 77 54 81 48 34
+
+// Output:
+// 2
+	
 #include<iostream>
 #include<math.h>
 int isPer(int num){
