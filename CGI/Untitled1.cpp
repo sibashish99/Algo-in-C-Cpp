44b29d074857e5bf27259bd908bbad66c673d38a
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-01-17 21:00:50 +0530

    Update Untitled1.cpp

--- CGI/Untitled1.cpp
+++ CGI/Untitled1.cpp
@@ -1,26 +1,34 @@
 #include<iostream>
 #include<math.h>
-using namespace std;
 int isPer(int num){
-	float sr;
-	sr=sqrt((double)num);
-	if((sr - floor(sr))==0)
-	   return 1;
-	else
-	   return 0;
+	int a;
+	float b;
+	
+	b=sqrt((double)num);
+	a=b;
+	
+	if(a==b){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+	
 }
+using namespace std;
 int main(){
 	int n;
+	int a[100];
 	cin>>n;
-	int a[1000000];
 	for(int i=0;i<n;i++){
 		cin>>a[i];
 	}
-	int c;
+	int c=0;
 	for(int i=0;i<n;i++){
-		if(isPer(a[i])==1){
-			c++;
-		}
+	   if(isPer(a[i])==1){
+	   	  c++;
+	   }
 	}
 	cout<<c<<endl;
+	
 }
